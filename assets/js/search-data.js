0ac2ed948d5df50f20f2fd2ad54b814d7e194516
emiliagyr <111396316+emiliagyr@users.noreply.github.com>
2026-04-27 12:40:48 +0000

    Deploying to gh-pages from @ emiliagyr/emiliagyr.github.io@92d2c1a0970a34269d394f21cd2bd1138de7e821 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,7 +66,12 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-a-simple-inline-announcement",
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
